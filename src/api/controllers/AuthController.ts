c8309056fc893ce190ffc5af0d2c35ba3716a28a
otienoelvis <sevileelvis@gmail.com>
2023-12-06 03:07:31 +0300

    2-factor Auth

--- src/api/controllers/AuthController.ts
+++ src/api/controllers/AuthController.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 import AuthService from '../services/AuthService';
 import { LoginType, SignUpType } from '../types/auth';
+import EmailService from '../services/EmailService';
 
 class AuthController {
     async login(
@@ -31,6 +32,57 @@ class AuthController {
             res.status(200).send({
                 message: 'Signed up successfully'
             });
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    async verifyCode(
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ): Promise<void> {
+        try {
+            const { email, verificationCode } = req.body;
+            const loginData = await EmailService.verifyCode(
+                email,
+                verificationCode
+            );
+
+            if (loginData) {
+                res.status(200).send({
+                    message: 'Code verification successful',
+                    data: loginData
+                });
+            } else {
+                res.status(400).send({
+                    message: 'Code verification failed'
+                });
+            }
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    async fetchUserLoginData(
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ): Promise<void> {
+        try {
+            const { email } = req.body;
+            const loginResponse = await AuthService.getUserLoginData(email);
+
+            if (loginResponse) {
+                res.status(200).send({
+                    message: 'Successfully signed in.',
+                    data: loginResponse
+                });
+            } else {
+                res.status(400).send({
+                    message: 'Code verification failed'
+                });
+            }
         } catch (error) {
             next(error);
         }
