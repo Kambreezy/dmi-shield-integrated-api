8f36d581dc86c3b97b30d311da55f203eeed3d2d
otienoelvis <sevileelvis@gmail.com>
2023-12-07 23:22:16 +0300

    change dialect config, added db sync files, Synchronously make dm migrations

--- src/database/sync.ts
+++ src/database/sync.ts
@@ -6,15 +6,11 @@ import User from '../api/models/User';
 import Document503 from '../api/models/Document503';
 import DocumentSari from '../api/models/DocumentSari';
 
-const syncTables = () =>
-    Promise.all([
-        User.sync(),
-        Role.sync(),
-        Document503.sync(),
-        DocumentSari.sync()
-    ]);
+const syncTables = () => {
+    User.sync();
+    Role.sync();
+    Document503.sync();
+    DocumentSari.sync();
+};
 
-syncTables()
-    .then((result) => console.log(result))
-    .catch((error) => console.log(error))
-    .finally(() => process.exit());
+syncTables();
